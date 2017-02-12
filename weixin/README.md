b908df87b1de75048599d49d87dda898213f9842
unknown <>
2017-02-12 23:59:06 +0800

    找了个参考的网页，建立栅格系统

--- weixin/README.md
+++ weixin/README.md
@@ -19,3 +19,7 @@ http://www.cnblogs.com/Leo_wl/p/4361289.html  实现注册登陆功能
 https://github.com/AllThingsSmitty/nodejs-chat 实时聊天系统
 
 http://router.vuejs.org/zh-cn/installation.html vue-router
+
+https://github.com/twbs/bootstrap-sass/blob/master/assets/stylesheets/bootstrap/mixins/_grid.scss
+
+https://www.licm.org/support-us/membership/
--- weixin/public/js/main.js
+++ weixin/public/js/main.js
@@ -1,87 +1,88 @@
-$(function () {
 
-	var dataManager = {
-		
-	}
-
-	window.ontouchmove = function (e) {
-		var evt = e || event;
-		evt.returnValue = false;
-	}
-	window.onmousemove = function (e) {
-		var evt = e || event;
-		evt.returnValue = false;
-	}
-
-
-	var questionCard = {
-		props: ['question'],
-		template:
-		'<div class="mui-panel qustion-card">' +
-			'<div class="content">{{question}}</div>' +
-			'<div class="record-area"></div>' +
-		'</div>'
-	};
-	var page = {
-		props: ['question'],
-		template:
-		'<section class="swiper-slide">' +
-			'<question-card v-bind:question="question"></question-card>' +
-		'</section>',
-		components: {
-			'question-card': questionCard
+Vue.component('nav-bar',{
+	template: 
+	'<div class="navigationBar">' +
+		'<div class="tab" v-on:click="clickTab">' +
+			'<span></span>' +
+			'<p>第一页</p>' +
+	'</div>' +
+		'<div class="tab" v-on:click="clickTab">' +
+			'<span></span>' +
+			'<p>第二页</p>' +
+		'</div>' +
+	'</div>',
+	methods: {
+		clickTab : function(event){
+			console.log(event.target);
 		}
+	}
+}) 
 
+// var dataManager = {
+
+// }
+
+var questionCard = {
+	props: ['question'],
+	template:
+	'<div class="mui-panel qustion-card">' +
+	'<div class="content">{{question}}</div>' +
+	'<div class="record-area"></div>' +
+	'</div>'
+};
+var page = {
+	props: ['question'],
+	template:
+	'<section class="swiper-slide">' +
+	'<question-card v-bind:question="question"></question-card>' +
+	'</section>',
+	components: {
+		'question-card': questionCard
 	}
 
-	var app = new Vue({
-		el: '#swiper_container',
-		data: {
-			questions: [
-				'第一个问题',
-				'第二个问题',
-				'第三个问题'
-			]
-		},
-		components: {
-			'page': page
-		}
+}
 
-	})
-
-	var swiper = new Swiper('.swiper-container', {
-		pagination: '.swiper-pagination',
-		paginationClickable: true
-	});
-
-
-
-
-
-	function record() {
-		var localId = "",
-			time_start = 0,
-			totla_t = 0,
-			timesUp;
-
-		wx.ready(function () {
-			$("record-area").on("touchstart", function (e) {
-				e.preventDeafult || e.preventDefault();
-				wx.startRecord();
-			}).on("touched", function () {
-				wx.stopRecord(function (res) {
-					localId = res.localId;
-				});
-			})
-		})
+var app = new Vue({
+	el: '#abc',
+	data: {
+		questions: [
+			'第一个问题',
+			'第二个问题',
+			'第三个问题'
+		]
 	}
-	record();
+	,
+	components: {
+		'page': page
+	}
+
+})
+
+// var swiper = new Swiper('.swiper-container', {
+// 	pagination: '.swiper-pagination',
+// 	paginationClickable: true
+// });
 
 
 
-	// //点击关注按钮
-	// $(".p3-attention").on("click",function(){
-	// 	window.open("http://mp.weixin.qq.com/s?__biz=MzA4MDk3MDgxOA==&mid=207747019&idx=1&sn=cb706c76dc0622695953adb7e223b297&scene=1&from=groupmessage&isappinstalled=0#rd");
-	// });
 
-});
+
+// function record() {
+// 	var localId = "",
+// 		time_start = 0,
+// 		totla_t = 0,
+// 		timesUp;
+
+// 	wx.ready(function () {
+// 		$("record-area").on("touchstart", function (e) {
+// 			e.preventDeafult || e.preventDefault();
+// 			wx.startRecord();
+// 		}).on("touched", function () {
+// 			wx.stopRecord(function (res) {
+// 				localId = res.localId;
+// 			});
+// 		})
+// 	})
+// }
+// record();
+
